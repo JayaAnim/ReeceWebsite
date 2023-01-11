9ef90ce4fe000dd3c3ab3d949a0636b903595d61
Chase Lamkin <chaselamkin@Chases-MacBook-Pro.local>
2023-01-10 20:02:19 -0600

    big fixes

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -2,34 +2,30 @@ import '../styles/About.css';
 import useOnScreen from '../hooks/useOnScreen';
 import { useRef, useEffect, useState } from 'react';
 import useNav from '../hooks/useNav';
-import paintstroke from '../img/paint-stroke.png';
 import { NavContext } from '../context/navContext';
 import { useContext } from 'react';
-import { CSSTransition } from 'react-transition-group';
-import { CgProfile } from 'react-icons/cg';
-import TypeWriter from 'typewriter-effect';
-
-const aboutString = 'As an aspiring actor, I am passionate about the art of storytelling and the power of performance to transport audiences to new worlds. I am dedicated to bringing characters to life and connecting with audiences on an emotional level through film, television, and theater. I am constantly working to improve my craft and take on new, challenging roles that allow me to showcase my range and versatility. I am confident that my passion, dedication, and hard work will take me far in this industry and I am excited to see where my journey as an actor will lead me.';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const About = () => {
-    const [displayFront, setDisplayFront] = useState(false);
-    const [flippedByView, setFlippedByView] = useState(false);
-    const [initTyping, setInitTyping] = useState(false);
-    const nodeRef = useRef(null);
+    const cardRef = useRef(null);
     const aboutRef = useNav('About');
     useOnScreen(aboutRef);
 
     const { activeLinkId } = useContext(NavContext);
     useEffect(() => {
-        if (activeLinkId === 'About' && !flippedByView) {
-            setDisplayFront(true);
-            setInitTyping(true);
-            setFlippedByView(true);
+        if (activeLinkId === 'About') {
+            cardRef.current.classList.add('in-view');
         }
-        else if (activeLinkId !== 'About') {
-            setFlippedByView(false);
+        else {
+            cardRef.current.classList.remove('in-view');
         }
-    }, [activeLinkId, flippedByView, displayFront, initTyping]);
+
+    }, [activeLinkId]);
+
+    useEffect(() => {
+        Aos.init({duration: 1000, once: false});
+    }, []);
 
     return (
 
@@ -39,35 +35,11 @@ const About = () => {
             </div>
             <div className='about-card-container'>
                 <div className='about-card-sub-container'>
-                    <CSSTransition
-                        nodeRef={nodeRef}
-                        in={displayFront}
-                        timeout={300}
-                        classNames='flip'
-                    >
-                        <div ref={nodeRef} className='about-card' onClick={() => setDisplayFront(!displayFront)}>
-                            <div className='about-card-front'>
-                                {/*
-                                {initTyping ?
-                                <TypeWriter
-                                    onInit={(typewriter) => {
-                                        typewriter
-                                        .pauseFor(1000)
-                                        .changeDelay(45)
-                                        .typeString(aboutString)
-                                        .start();
-                                    }}
-                                /> : null}
-                                */}
-                                As an aspiring actor, I am passionate about the art of storytelling and the power of performance to transport audiences to new worlds. I am dedicated to bringing characters to life and connecting with audiences on an emotional level through film, television, and theater. I am constantly working to improve my craft and take on new, challenging roles that allow me to showcase my range and versatility. I am confident that my passion, dedication, and hard work will take me far in this industry and I am excited to see where my journey as an actor will lead me.
-                                
-                            </div>
-                            <div className='about-card-back'>
-                                <CgProfile className='about-icon'/>
-                            </div>
-
-                        </div>
-                    </CSSTransition>
+                    <div ref={cardRef} className='about-card'>
+                        <div className='top-line'/>
+                        <div className='bottom-line'/>
+                        <div data-aos='zoom-out'>As an aspiring actor, I am passionate about the art of storytelling and the power of performance to transport audiences to new worlds. I am dedicated to bringing characters to life and connecting with audiences on an emotional level through film, television, and theater. I am constantly working to improve my craft and take on new, challenging roles that allow me to showcase my range and versatility. I am confident that my passion, dedication, and hard work will take me far in this industry and I am excited to see where my journey as an actor will lead me.</div> 
+                    </div>
                 </div>
             </div>
         </section>
